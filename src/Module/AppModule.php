7ff7c3998b9bd2e54ae830694e52c5b3b71a2fd8
Yuki Adachi <yuki777@gmail.com>
2021-07-15 21:25:33 +0900

    Sample sql

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -7,6 +7,8 @@ namespace MyVendor\Weekday\Module;
 use BEAR\Dotenv\Dotenv;
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
+use Ray\AuraSqlModule\AuraSqlModule;
+use Ray\Query\SqlQueryModule;
 
 use function dirname;
 
@@ -15,6 +17,8 @@ class AppModule extends AbstractAppModule
     protected function configure(): void
     {
         (new Dotenv())->load(dirname(__DIR__, 2));
+        $this->install(new AuraSqlModule('sqlite:' . $this->appMeta->appDir . '/var/db/sample.sqlite3'));
+        $this->install(new SqlQueryModule($this->appMeta->appDir . '/var/db/sql'));
         $this->install(new PackageModule());
     }
 }
--- src/Resource/Page/Index.php
+++ src/Resource/Page/Index.php
@@ -5,17 +5,33 @@ declare(strict_types=1);
 namespace MyVendor\Weekday\Resource\Page;
 
 use BEAR\Resource\ResourceObject;
+use Ray\IdentityValueModule\NowInterface;
+use Ray\Di\Di\Inject;
+use Ray\Di\Di\Named;
 
 class Index extends ResourceObject
 {
     /** @var array{greeting: string} */
     public $body;
 
+    /** @var callable */
+    private $selectOne;
+
+    /**
+     * @Inject
+     * @Named("selectOne=selectOne")
+     */
+    public function __construct(callable $selectOne)
+    {
+        $this->selectOne = $selectOne;
+    }
+
     /** @return static */
     public function onGet(string $name = 'BEAR.Sunday')
     {
         $this->body = [
             'greeting' => 'Hello ' . $name,
+            'one' => ($this->selectOne)([]),
         ];
 
         return $this;
